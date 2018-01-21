40e2a295b2ee791fb42b6ff1cb9a6e0a6c876b6b
essvee <s.vincent@nhm.ac.uk>
2018-01-21 14:51:01 +0000

    first commit

--- exercise-three/src/SumOfMultiples.kt
+++ exercise-three/src/SumOfMultiples.kt
@@ -1,4 +1,15 @@
 object SumOfMultiples {
 
-    fun sum(factors: Set<Int>, limit: Int): Int = 0
+    fun sum(factors: Set<Int>, limit: Int): Int {
+        var total = 0
+        for (i in 1..(limit -1)) {
+            for (f in factors) {
+                if(i % f == 0) {
+                    total += i
+                    break
+                }
+            }
+        }
+        return total
+    }
 }
