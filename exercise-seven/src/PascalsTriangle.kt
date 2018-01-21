8f35f1efc638051480667b6873dbbdcb97c5e095
essvee <s.vincent@nhm.ac.uk>
2018-01-21 21:40:42 +0000

    Passes all tests

--- exercise-seven/src/PascalsTriangle.kt
+++ exercise-seven/src/PascalsTriangle.kt
@@ -1,7 +1,19 @@
 object PascalsTriangle {
-
     fun computeTriangle(rows: Int): List<List<Int>> {
-        // TODO
-        return listOf(listOf<Int>())
+        if (rows < 0) throw IllegalArgumentException("Rows can't be negative!")
+        val resultList: MutableList<List<Int>> = mutableListOf()
+        if (rows == 0) return resultList
+        resultList.add(listOf(1))
+
+        for (n in 1 until rows) {
+            val tempList: MutableList<Int> = mutableListOf(1)
+            for (i in 1 until n) {
+                val lastRow = resultList[resultList.size - 1]
+                tempList.add(lastRow[(i - 1) + lastRow[i]])
+            }
+            tempList.add(1)
+            resultList.add(tempList)
+        }
+        return resultList
     }
 }
