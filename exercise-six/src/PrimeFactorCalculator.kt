4b3869c9985a889862686b607e16217cb9de62c0
essvee <s.vincent@nhm.ac.uk>
2018-01-21 19:58:37 +0000

    Int function passes tests

--- exercise-six/src/PrimeFactorCalculator.kt
+++ exercise-six/src/PrimeFactorCalculator.kt
@@ -1,12 +1,23 @@
 object PrimeFactorCalculator {
 
     fun primeFactors(int: Int): List<Int> {
-        // TODO
-        return listOf<Int>()
+        val resultList :MutableList<Int> = mutableListOf()
+        var divisor = 2
+        var n = int
+
+        while (n > 1) {
+            if (n % divisor == 0) {
+                n /= divisor
+                resultList.add(divisor)
+            } else {
+                divisor++
+            }
+        }
+        return resultList
     }
 
     fun primeFactors(long: Long): List<Long> {
-        // TODO
-        return listOf<Long>()
+
+        return listOf()
     }
 }
