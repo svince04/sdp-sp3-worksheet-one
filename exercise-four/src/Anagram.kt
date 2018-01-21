29ac2742cf36fad051c7d168ed9e39111577228e
essvee <s.vincent@nhm.ac.uk>
2018-01-21 16:48:37 +0000

    Passing all tests

--- exercise-four/src/Anagram.kt
+++ exercise-four/src/Anagram.kt
@@ -1,6 +1,25 @@
 class Anagram(private val word: String) {
 
     fun match(anagrams: Collection<String>): Set<String> {
-        return setOf<String>()
+        val result :Set<String> = anagrams.filter { anag -> (anag.length == word.length) }
+                .toMutableSet()
+        val matches :MutableSet<String> = mutableSetOf()
+        val targetWord = normaliseAndSort(word)
+
+        for (w in result) {
+            if (word.toLowerCase() == w.toLowerCase()) {
+                continue
+            }
+            if (normaliseAndSort(w) == targetWord) matches.add(w)
+        }
+        return matches
     }
+
+    private fun normaliseAndSort(input: String) :String {
+        val inputCharArray = input.toLowerCase().toCharArray()
+        inputCharArray.sort()
+        return inputCharArray.joinToString("")
+
+    }
+
 }
